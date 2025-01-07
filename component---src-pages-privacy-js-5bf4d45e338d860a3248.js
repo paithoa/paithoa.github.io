2ad7e8a3fc8d413e4fe660c9317efc83861136d1
Handy <hhasan@4dx.com>
2025-01-07 13:28:16 +1100

    Updates

---
+++ component---src-pages-privacy-js-5bf4d45e338d860a3248.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[745],{1971:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(6540),a=n(5927);function l(){return r.createElement(r.Fragment,null,r.createElement(a.G$,{title:"Privacy Policy",useTitleTemplate:!0,noIndex:!0}),r.createElement(a.YW,null,r.createElement(a.v3,{sectionId:"privacy",heading:"Privacy Policy"})))}}}]);
+//# sourceMappingURL=component---src-pages-privacy-js-5bf4d45e338d860a3248.js.map
