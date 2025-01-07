bef80c54c660871a647ec3b5ca854a2a39c3e75c
Handy <hhasan@4dx.com>
2025-01-07 12:46:16 +1100

    Add GitHub Pages deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,10 +17,6 @@ export default function IndexPage() {
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
       </Page>
     </>
   );
